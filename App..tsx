156239068090ddea5fe0b58b5fc50418077162c9
Asam237 <abbasalimokolo@gmail.com>
2021-10-15 15:44:01 +0100

    add files

---
+++ App..tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Hello Alert Eneo</Text>
+    </View>
+  );
+};

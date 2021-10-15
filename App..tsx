0ad1c982fd4dd0740f271270e2b654b77765fd5a
Asam237 <abbasalimokolo@gmail.com>
2021-10-15 15:50:18 +0100

    Add screen file and architecture

--- App..tsx
+++ App..tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {Welcome} from './src/screens/welcome/Welcome.screen';
 
 export const App = () => {
-  return (
-    <View>
-      <Text>Hello Alert Eneo</Text>
-    </View>
-  );
+  return <Welcome />;
 };
